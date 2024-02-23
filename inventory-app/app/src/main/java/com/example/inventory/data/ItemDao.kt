c8c7b2dbef270a2cd9fbccf6b2146b5613cffe40
Ethan Chang <ethanysc@Ethans-MacBook-Pro.local>
2024-02-23 17:50:13 +0800

    Add getItem and getAllItems custom queries, return Flow

--- inventory-app/app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ inventory-app/app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -4,7 +4,9 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ItemDao {
@@ -16,4 +18,10 @@ interface ItemDao {
 
     @Delete
     suspend fun delete(item: Item)
+
+    @Query("SELECT * from items WHERE id = :id")
+    fun getItem(id: Int): Flow<Item>
+
+    @Query("SELECT * from items ORDER BY name ASC")
+    fun getAllItems(): Flow<List<Item>>
 }
