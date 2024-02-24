3760664cdab2874eca5adf1b4019ed797c310eb8
Ethan Chang <ethanysc@Ethans-MacBook-Pro.local>
2024-02-24 19:49:15 +0800

    Add entitiy def, DAO, repo and offline override repo for Airport and Favorite. Create Database def and initial AppContainer injection

--- flight-search/app/src/main/java/com/example/flightsearch/data/AppContainer.kt
+++ flight-search/app/src/main/java/com/example/flightsearch/data/AppContainer.kt
@@ -1,13 +1,16 @@
 package com.example.flightsearch.data
 
 import android.content.Context
+import com.example.flightsearch.data.airport.AirportsRepository
+import com.example.flightsearch.data.favorite.FavoritesRepository
 
 interface AppContainer {
     val airportRepository: AirportsRepository
+    val favoriteRepository: FavoritesRepository
 }
 
-class AppDataContainer(private val context: Context) : AppContainer {
-    override val airportRepository: AirportsRepository by lazy {
-        OfflineAirportssRepository(AirportsDatabase.getDatabase(context).airportsDao())
-    }
-}
+//class AppDataContainer(private val context: Context) : AppContainer {
+//    override val airportRepository: AirportsRepository by lazy {
+//        OfflineAirportssRepository(AirportsDatabase.getDatabase(context).airportsDao())
+//    }
+//}
--- flight-search/app/src/main/java/com/example/flightsearch/data/FlightDatabase.kt
+++ flight-search/app/src/main/java/com/example/flightsearch/data/FlightDatabase.kt
@@ -1,4 +1,33 @@
 package com.example.flightsearch.data
 
-class FlightDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.flightsearch.data.airport.Airport
+import com.example.flightsearch.data.airport.AirportDao
+import com.example.flightsearch.data.favorite.Favorite
+import com.example.flightsearch.data.favorite.FavoriteDao
+
+@Database(
+    entities = [Airport::class, Favorite::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class FlightDatabase: RoomDatabase() {
+
+    abstract fun airportDao(): AirportDao
+    abstract fun favoriteDao(): FavoriteDao
+
+    companion object {
+        @Volatile
+        private var Instance: FlightDatabase? = null
+
+        fun getDatabase(context: Context): FlightDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, FlightDatabase::class.java, "flight_database")
+                    .build().also { Instance = it }
+            }
+        }
+    }
 }
