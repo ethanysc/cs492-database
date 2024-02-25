cde1f5e4945e3f85acb46037a56f277eb934de1c
Ethan Chang <ethanysc@Ethans-MacBook-Pro.local>
2024-02-25 23:51:52 +0800

    Modify AirportViewModel to save UiState vals searchString and isSelectingDestination. Wire up FlightSearchInput and FlightSearchAutocomplete interactions.

--- flight-search/app/src/main/java/com/example/flightsearch/ui/AirportViewModel.kt
+++ flight-search/app/src/main/java/com/example/flightsearch/ui/AirportViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.flightsearch.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
@@ -9,8 +12,39 @@ import com.example.flightsearch.FlightSearchApplication
 import com.example.flightsearch.data.Airport
 import com.example.flightsearch.data.AirportDao
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class AirportViewModel(private val airportDao: AirportDao): ViewModel() {
+
+    private val _uiState = MutableStateFlow(AirportUiState())
+    val airportUiState: StateFlow<AirportUiState> = _uiState.asStateFlow()
+
+    init {
+        initializeUIState()
+    }
+
+    private fun initializeUIState() {
+        _uiState.value = AirportUiState()
+    }
+
+    fun updateSearchString(searchString: String) {
+        _uiState.update { currentState ->
+            currentState.copy(
+                searchString = searchString
+            )
+        }
+    }
+    fun toggleIsSelectingDestination() {
+        _uiState.update { currentState ->
+            currentState.copy(
+                isSelectingDestination = currentState.isSelectingDestination == false
+            )
+        }
+    }
+
     fun searchAirports(searchString: String): Flow<List<Airport>> = airportDao.searchAirports(searchString)
     fun getFlightsByAirportId(id: Int): Flow<List<Airport>> = airportDao.getFlightsByAirportId(id)
 
@@ -23,6 +57,11 @@ class AirportViewModel(private val airportDao: AirportDao): ViewModel() {
         }
     }
 }
+
+data class AirportUiState(
+    val searchString: String = "",
+    val isSelectingDestination: Boolean = false,
+)
 
 //package com.example.flightsearch.ui.airport
 //
--- flight-search/app/src/main/java/com/example/flightsearch/ui/FlightSearchApp.kt
+++ flight-search/app/src/main/java/com/example/flightsearch/ui/FlightSearchApp.kt
@@ -1,21 +1,172 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.flightsearch.R
+import com.example.flightsearch.data.Airport
 import com.example.flightsearch.ui.AirportViewModel
 import com.example.flightsearch.ui.FavoriteViewModel
+import com.example.flightsearch.ui.theme.FlightSearchTheme
+import kotlinx.coroutines.launch
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FlightSearchApp (
-    modifier: Modifier = Modifier,
     airportViewModel: AirportViewModel = viewModel(factory = AirportViewModel.factory),
-    favoriteViewModel: FavoriteViewModel = viewModel(factory = FavoriteViewModel.factory)
+    favoriteViewModel: FavoriteViewModel = viewModel(factory = FavoriteViewModel.factory),
+    modifier: Modifier = Modifier
 ) {
-    val airportsList by airportViewModel.searchAirports("").collectAsState(emptyList())
-    val favoriteList by favoriteViewModel.getAllFavorites().collectAsState(emptyList())
+    val topAppBarTitle = stringResource(R.string.top_bar_title)
 
-    Text(text = "Hello")
+    val airportUiState = airportViewModel.airportUiState.collectAsState()
+    val searchString = airportUiState.value.searchString
+    val isSelectingDestination = airportUiState.value.isSelectingDestination
+
+    val airportsList by airportViewModel.searchAirports(searchString).collectAsState(emptyList())
+    val favoritesList by favoriteViewModel.getAllFavorites().collectAsState(emptyList())
+
+    Scaffold(
+        topBar = { FlightSearchTopAppBar(title = topAppBarTitle) }
+    ) { innerPadding ->
+        Column (
+            modifier = Modifier
+                .padding(innerPadding)
+                .background(Color(0x1F5FA6))
+        ) {
+            FlightSearchInput(
+                searchString = searchString,
+                viewModel = airportViewModel
+            )
+            if (searchString != "") { // Render autocomplete suggestions
+                FlightSearchAutoComplete(
+                    airportsList = airportsList,
+                    viewModel = airportViewModel
+                )
+            } else if (isSelectingDestination) { // Render flight suggestions
+                Text(text = "Hello here are the flight suggestions")
+            } else { // Render Favorites
+                Text(text = "Favorites")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FlightSearchTopAppBar (title: String) {
+    TopAppBar(
+        title = { Text(title) },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+        )
+    )
+}
+
+@Composable
+fun FlightSearchInput (
+    searchString: String = "",
+    viewModel: AirportViewModel,
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val placeholder = stringResource(R.string.flight_search_placeholder_label)
+    OutlinedTextField(
+        value = searchString,
+        onValueChange = {
+            coroutineScope.launch {
+                viewModel.updateSearchString(it)
+            }
+        },
+        placeholder = { Text(placeholder) },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+            unfocusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+            disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+        ),
+        leadingIcon = { Icon(imageVector = Icons.Outlined.Search, contentDescription = null) },
+        shape = RoundedCornerShape(75),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(18.dp),
+        singleLine = true
+    )
+}
+
+@Composable
+fun FlightSearchAutoComplete(
+    airportsList: List<Airport> = emptyList(),
+    viewModel: AirportViewModel,
+) {
+    val coroutineScope = rememberCoroutineScope()
+    LazyColumn(
+        modifier = Modifier.padding(horizontal = 18.dp, vertical = 16.dp),
+    ) {
+        items(airportsList) { airport ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable() {
+                        coroutineScope.launch {
+                            viewModel.getFlightsByAirportId(airport.id)
+                            viewModel.toggleIsSelectingDestination()
+                        }
+                    }
+                    .padding(4.dp),
+            ) {
+                Text(
+                    text = airport.iata_code,
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 14.sp
+                    ),
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+                Text(
+                    text = airport.name,
+                    style = TextStyle(
+                        fontSize = 14.sp
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun FlightSearchApp() {
+    FlightSearchTheme {
+        FlightSearchApp()
+    }
 }
